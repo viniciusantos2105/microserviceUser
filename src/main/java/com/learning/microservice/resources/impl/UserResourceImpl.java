5d33dfdbc05576a68a02323a592476c01fc25e69
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2022-10-26 13:41:02 -0300

    add load balancing

--- src/main/java/com/learning/microservice/resources/impl/UserResourceImpl.java
+++ src/main/java/com/learning/microservice/resources/impl/UserResourceImpl.java
@@ -4,12 +4,15 @@ import com.learning.microservice.domain.User;
 import com.learning.microservice.resources.UserResource;
 import com.learning.microservice.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
@@ -17,8 +20,11 @@ public class UserResourceImpl implements UserResource {
 
     private final UserService service;
 
+    private final Environment env;
+
     @Override
     public ResponseEntity<User> findById(Long id) {
+        log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + "port");
         return ResponseEntity.ok().body(service.findById(id));
     }
 
